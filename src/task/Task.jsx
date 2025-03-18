4987cc7e1b47d34a38e11dacaacd703a71d19681
seribucode <alfin.md17@gmail.com>
2025-03-18 23:48:09 +0200

    bab use immer

---
+++ src/task/Task.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useImmer } from "use-immer";
+
+export default function Task(){
+  const [item, setItem] = useState("");
+  const [items, setItems] = useImmer([]);
+
+  function handleChange(e){
+    setItem(e.target.value)
+  }
+
+  function handleClick(e){
+    e.preventDefault();
+    setItems((draft)=>{
+      draft.push(item);
+    })
+    setItem("")
+  }
+  return(
+    <div>
+      <h1>Create Task</h1>
+      <input value={item} onChange={handleChange}/>
+      <button onClick={handleClick}>Add</button>
+      <h1>List Task</h1>
+      <ul>
+        {items.map((item, index) =>
+          <li key={index}>{item}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
